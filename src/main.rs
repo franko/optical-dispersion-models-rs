b07dd6cfedca4d1a16420168e66d0d7e494f79a0
Francesco Abbate <francesco.bbt@gmail.com>
2017-01-30 23:09:51 +0100

    Develop more computations with constants

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 extern crate num;
 use num::complex::Complex64 as complex;
+use std::ops::Mul;
 
 struct Oscillator {
-    nosc: f64,
-    en:   f64,
-    eg:   f64,
-    nu:   f64,
-    phi:  f64,
+    pub nosc: f64,
+    pub en:   f64,
+    pub eg:   f64,
+    pub phi:  f64,
+}
+
+const I: complex = complex{re: 0.0, im: 1.0};
+
+fn sqr<T> (x: T) -> T where T: Mul<Output=T> + Copy { x * x }
+
+impl Oscillator {
+    fn value(&self, wavelength: f64) -> complex {
+        let e = 1240.0 / wavelength;
+        self.nosc / (sqr(self.en) - sqr(e) + I * self.eg * e)
+    }
 }
 
 struct HODispersion {
-    oscillators: Vec<Oscillator>
+    pub oscillators: Vec<Oscillator>
 }
 
-trait dispersion {
-    fn getN(&self, wavelength: f64) -> complex;
+trait Dispersion {
+    fn get_n(&self, wavelength: f64) -> complex;
 }
 
-impl dispersion for HODispersion {
-    fn getN(&self, wavelength: f64) -> complex {
-        self.oscillators.iter().fold(complex{re: 0.0, im: 0.0}, |n,osc| n + osc.nosc)
+impl Dispersion for HODispersion {
+    fn get_n(&self, wavelength: f64) -> complex {
+        let eps = self.oscillators.iter().fold(complex{re: 0.0, im: 0.0}, |eps, osc| eps + osc.value(wavelength));
+        eps.sqrt()
     }
 }
 
 fn main() {
-    println!("Hello!");
+    let d = HODispersion{oscillators: vec![Oscillator{nosc: 15.2, en: 15.7, eg: 0.1, phi: 0.0}]};
+    println!("Hello {}!", d.get_n(633.0));
 }
